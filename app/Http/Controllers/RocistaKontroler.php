a24ce394b6893c487c15934e0dfd506df6d9c31b
chana-chh <chanasrb@gmail.com>
2017-08-25 10:10:35 +0200

    utpm

--- app/Http/Controllers/RocistaKontroler.php
+++ app/Http/Controllers/RocistaKontroler.php
@@ -8,7 +8,7 @@ use Redirect;
 use Gate;
 
 use App\Modeli\Predmet;
-use App\Modeli\Tiprocista;
+use App\Modeli\TipRocista;
 use App\Modeli\Rociste;
 
 class RocistaKontroler extends Kontroler
@@ -32,11 +32,60 @@ class RocistaKontroler extends Kontroler
         $rociste->vreme = $r->vreme;
         $rociste->tip_id = $r->tip_id;
         $rociste->opis = $r->opis;
-        $rociste->predmet_id = $r->dodaj_id;
+        $rociste->predmet_id = $r->predmet_id;
 
         $rociste->save();
         
         Session::flash('uspeh','Рочиште је успешно додато!');
-        return redirect()->route('rocista');
+        return redirect()->route('predmeti.pregled', $r->predmet_id);
+    }
+
+    public function getDodavanje()
+    {
+        $predmeti = Predmet::all();
+        $tipovi_rocista = TipRocista::all();
+        return view('rocista_dodavanje')->with(compact ('predmeti', 'tipovi_rocista'));
+    }
+
+    public function getDetalj(Request $r)
+        {
+            if($r->ajax()){
+                $id = $r->id;
+                $rociste = Rociste::find($id);
+                $tipovi_rocista = TipRocista::all();
+                return response()->json(array('rociste'=>$rociste,'tipovi_rocista'=>$tipovi_rocista));
+            }
+        }
+
+        public function postIzmena(Request $r)
+        {
+            $this->validate($r, [
+                'datumm' => ['required'],
+                'vremem' => ['required']
+            ]);
+
+                $id = $r -> edit_id;
+                $rociste = Rociste::find($id);
+                $rociste -> datum = $r -> datumm;
+                $rociste->vreme = $r->vremem;
+                $rociste->tip_id = $r->tip_idm;
+
+                $rociste -> save();
+
+            Session::flash('uspeh','Детаљи рочишта су успешно измењени!');
+            return Redirect::back();
+        }
+
+    public function postBrisanje(Request $r)
+    {   
+                $id = $r->id;
+                $rociste = Rociste::find($id);
+                $odgovor = $rociste->delete();
+                if ($odgovor) {
+                Session::flash('uspeh','Рочиште је успешно обрисано!');
+                }
+                else{
+                Session::flash('greska','Дошло је до грешке приликом брисања рочишта. Покушајте поново, касније!');
+                }
     }
 }
