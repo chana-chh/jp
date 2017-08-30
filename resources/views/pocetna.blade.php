78228d8a960e1a647f167055fc11e1d4e800d6d8
kure-kg <stashakg@gmail.com>
2017-08-30 09:32:15 +0200

    chart proba

--- resources/views/pocetna.blade.php
+++ resources/views/pocetna.blade.php
@@ -89,11 +89,52 @@
                     </a>
                 </div>
                 <div class="panel-footer text-center">
-                    <h3>
-                        Овде иде неки износ.
-                    </h3>
+                  <canvas id="myChart"></canvas>
                 </div>
             </div>
         </div>
 </div>
+@endsection
+
+@section('skripte')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.js"></script>
+<script>
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Септембар", "Октобар", "Новембар", "Децембар", "Јануар", "Фебруар"],
+        datasets: [{
+            label: 'Биланс',
+            data: [120000, 19000, 73000, 50000, 42000, 83000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
 @endsection
