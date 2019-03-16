e7b9fb280100853c6b8573f3c43b30650ba468aa
kure-kg <stashakg@gmail.com>
2019-03-16 11:40:00 +0100

    Nova tabela predmeti korekcija

--- resources/views/predmeti.blade.php
+++ resources/views/predmeti.blade.php
@@ -206,8 +206,59 @@
 <div class="row">
     <div class="col-md-12">
         <div class="table-responsive">
-            <table class="table table-striped table-condensed tabelaPredmeti" name="tabelaPredmeti" id="tabelaPredmeti">
-                @include('sabloni.inc.supertabela')
+            <table class="table table-striped table-condensed">
+              <thead>
+    <tr>
+        <th style="width: 3%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="id">
+            # <span id="id_icon"></span></th>
+        <th style="width: 7%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="st_naziv">
+            Статус <span id="st_naziv_icon"></span></th>
+        <th style="width: 8%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="ceo_broj_predmeta">
+            Број <span id="ceo_broj_predmeta_icon"></span></th>
+        <th style="width: 18%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="stranka_1">
+            Тужилац <span id="stranka_1_icon"></span></th>
+        <th style="width: 18%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="stranka_2">
+            Тужени <span id="stranka_2_icon"></span></th>
+        <th style="width: 9%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="sudbroj">
+            Надлежни орган бр. <span id="sudbroj_icon"></span></th>
+        <th style="width: 7%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="opis_kp">
+            КП <span id="opis_kp_icon"></span></th>
+        <th style="width: 8%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="opis_adresa">
+            Адреса <span id="opis_adresa_icon"></span></th>
+        <th style="width: 14%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="vp_naziv">
+            Врста предмета <span id="vp_naziv_icon"></span></th>
+        <th style="width: 5%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="datum_tuzbe">
+            Датум <span id="datum_tuzbe_icon"></span></th>
+        <th style="width: 3%"><i class="fa fa-cogs"></i></th>
+    </tr>
+</thead>
+<tbody name="tabelaPredmeti" id="tabelaPredmeti">
+  @include('sabloni.inc.supertabela')
+  </tbody>
+                <tfoot id="linkovi">
+    <tr height="30px"></tr>
+    <tr>
+        <td colspan="5">
+            {!! $linkovi['buttons'] !!}
+        </td>
+        <td colspan="2">
+            <div class="col-md-6" style="margin-top: 20px">
+                Иди на страну:
+            </div>
+            <div class="col-md-6" style="margin-top: 10px">
+                {!! $linkovi['select'] !!}
+            </div>
+        </td>
+        <td colspan="4" class="text-right" style="padding: 20px">
+            <p>
+                Приказани редови од
+                <em>{{ $linkovi['row_from'] }}</em> до
+                <em>{{ $linkovi['row_to'] }}</em> од укупно
+                <em>{{ $linkovi['total_rows'] }}</em>
+            </p>
+        </td>
+    </tr>
+</tfoot>
             </table>
             <input type="hidden" name="hidden_page" id="hidden_page" value="1" />
             <input type="hidden" name="hidden_column_name" id="hidden_column_name" value="id" />
--- resources/views/sabloni/inc/supertabela.blade.php
+++ resources/views/sabloni/inc/supertabela.blade.php
@@ -1,32 +1,7 @@
 @if(count($podaci) === 0)
 <h2 class="text-warning text-center">Нема резултата за задате критеријуме.</h3>
 @else
-<thead>
-    <tr>
-        <th style="width: 3%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="id">
-            # <span id="id_icon"></span></th>
-        <th style="width: 7%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="st_naziv">
-            Статус <span id="st_naziv_icon"></span></th>
-        <th style="width: 8%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="ceo_broj_predmeta">
-            Број <span id="ceo_broj_predmeta_icon"></span></th>
-        <th style="width: 18%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="stranka_1">
-            Тужилац <span id="stranka_1_icon"></span></th>
-        <th style="width: 18%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="stranka_2">
-            Тужени <span id="stranka_2_icon"></span></th>
-        <th style="width: 9%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="sudbroj">
-            Надлежни орган бр. <span id="sudbroj_icon"></span></th>
-        <th style="width: 7%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="opis_kp">
-            КП <span id="opis_kp_icon"></span></th>
-        <th style="width: 8%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="opis_adresa">
-            Адреса <span id="opis_adresa_icon"></span></th>
-        <th style="width: 14%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="vp_naziv">
-            Врста предмета <span id="vp_naziv_icon"></span></th>
-        <th style="width: 5%; cursor: pointer" class="sorting" data-sorting_type="asc" data-column_name="datum_tuzbe">
-            Датум <span id="datum_tuzbe_icon"></span></th>
-        <th style="width: 3%"><i class="fa fa-cogs"></i></th>
-    </tr>
-</thead>
-<tbody>
+
     @foreach($podaci as $row)
     <tr>
         <td>{{ $row->id}}</td>
@@ -46,29 +21,4 @@
         <td><a class="btn btn-success btn-xs otvori_predmet" id="dugmePredmet" title="{{ $row->puno_ime }}" href="{{ route('predmeti.pregled', $row->id) }}"><i class="fa fa-eye"></i></a></td>
     </tr>
     @endforeach
-</tbody>
-<tfoot id="linkovi">
-    <tr height="30px"></tr>
-    <tr>
-        <td colspan="5">
-            {!! $linkovi['buttons'] !!}
-        </td>
-        <td colspan="2">
-            <div class="col-md-6" style="margin-top: 20px">
-                Иди на страну:
-            </div>
-            <div class="col-md-6" style="margin-top: 10px">
-                {!! $linkovi['select'] !!}
-            </div>
-        </td>
-        <td colspan="4" class="text-right" style="padding: 20px">
-            <p>
-                Приказани редови од
-                <em>{{ $linkovi['row_from'] }}</em> до
-                <em>{{ $linkovi['row_to'] }}</em> од укупно
-                <em>{{ $linkovi['total_rows'] }}</em>
-            </p>
-        </td>
-    </tr>
-</tfoot>
 @endif
